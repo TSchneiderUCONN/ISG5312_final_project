73e7999a92be46eb3ad2d8d518f1897c9bab39f0
Tyce Schneider <rnu24002@uconn.edu>
2025-05-12 06:25:07 -0400

    added variant calling scripts

---
+++ scripts/05_variantCalling/03_bcftools.sh
@@ -0,0 +1,48 @@
+#!/bin/bash 
+#SBATCH --job-name=bcftools_ISG5312
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 60
+#SBATCH --mem=100G
+#SBATCH --qos=general
+#SBATCH --partition=general
+#SBATCH --mail-user=
+#SBATCH --mail-type=ALL
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+hostname
+date
+
+#Load software
+module load bcftools/1.19
+module load htslib/1.19.1
+module load parallel/20180122
+module load vcflib/1.0.13
+
+#Specify input/output directories
+INDIR=../../results/03_Alignment/bwa_align/ 
+OUTDIR=../../results/05_variantCalling/bcftools
+	mkdir -p ${OUTDIR}
+
+#Make a list of bam files
+ls ${INDIR}/*.bam >${INDIR}/bam_list.txt
+
+#Set reference genome location
+GEN=../../genome/GCF_003597395.1_ASM359739v1_genomic.fna
+
+#Identify regions file
+regionsfile=../../genome/100kbWin.bed
+
+#Call variants in parallel
+(cat "$regionsfile" | parallel -k -j 30 \
+ "bcftools mpileup -f ${GEN} -b ${INDIR}/bam_list.txt -q 25 -Q 25 -r {} | bcftools call -m -v"
+) | 
+vcffirstheader |
+vcfstreamsort -w 1000 |
+vcfuniq |
+bgzip >${OUTDIR}/galapagos.vcf.gz
+
+#Index vcf
+tabix -p vcf ${OUTDIR}/galapagos.vcf.gz
+
