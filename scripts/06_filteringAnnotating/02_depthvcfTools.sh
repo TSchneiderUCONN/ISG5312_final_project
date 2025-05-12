e127a0979cdb9c6a23c8f6a185b3858f6228512c
Tyce Schneider <rnu24002@uconn.edu>
2025-05-12 10:14:12 -0400

    Update 02_depthvcfTools.sh

--- scripts/06_filteringAnnotating/02_depthvcfTools.sh
+++ scripts/06_filteringAnnotating/02_depthvcfTools.sh
@@ -18,7 +18,7 @@ date
 module load vcftools/0.1.16
 
 #Set variables
-VCF=../../results/05_variantCalling/filtered/testgalapagosFiltered.vcf.recode.vcf
+VCF=../../results/05_variantCalling/filtered/galapagosFiltered.vcf.recode.vcf
 OUTDIR=../../results/05_variantCalling/filtered
 
 #Calculate mean depth per individual for filtered file
